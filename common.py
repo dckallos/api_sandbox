458d661cfab6795378af318ddf73192dcbc45047
Dan Kalleward <d.kalleward@gmail.com>
2018-02-25 17:20:20 -0500

    Cleaner version of code from multiple API calls

--- common.py
+++ common.py
@@ -2,22 +2,26 @@ import json
 import os
 
 ## Import URL Information
-def get_endpoints():
+def import_endpoints():
     cfg = json.load(open(os.getcwd()+'/config/configure_endpoints.json'))
     google_endpoint = cfg['GOOGLE']
     foursquare_endpoint = cfg['FOURSQUARE']
     return(google_endpoint, foursquare_endpoint)
 
-def get_google_key():
+def import_google_key():
     cfg = json.load(open(os.getcwd()+'/config/configure_tokens.json'))
     google_api_key = cfg['ENDPOINTS'][0]['GOOGLE']['GEOCODE']
     return(google_api_key)
 
-def get_foursquare_keys():
+def import_foursquare_keys():
     cfg = json.load(open(os.getcwd()+'/config/configure_tokens.json'))
     fs_client_id = cfg['ENDPOINTS'][1]['FOURSQUARE']['CLIENT_ID']
     fs_client_secret = cfg['ENDPOINTS'][1]['FOURSQUARE']['CLIENT_SECRET']
     return(fs_client_id, fs_client_secret)
 
-print get_foursquare_keys()
+def parse_coordinates(google_json_response):
+    lat = google_json_response['results'][0]['geometry']['location']['lat']
+    lng = google_json_response['results'][0]['geometry']['location']['lng']
+    coord_string = '%s,%s' % (lat, lng)
+    return(coord_string)
     
--- mashup.py
+++ mashup.py
@@ -3,12 +3,12 @@ import requests
 import httplib2
 import common
 
-def get_geocode_location(query_string):
+def define_geocode_location(location_string):
     
-    location_string = query_string.replace(' ','+')
-    url = common.get_endpoints()[0]
+    location_string = location_string.replace(' ','+')
+    url = common.import_endpoints()[0]
     full_url = (url+'?address=%s&key=%s' % (
-        location_string, common.get_google_key()
+        location_string, common.import_google_key()
     ))
     h = httplib2.Http()
     [response, content] = h.request(full_url, 'GET')
@@ -16,30 +16,24 @@ def get_geocode_location(query_string):
     #print('response header: %s \n \n' % response)
     return result
 
-x = get_geocode_location('Kalamazoo,MI')
-#print x['results'][]
-#print json.dumps(x, sort_keys=True, indent=4)
-#TODO: ENSURE THAT THIS IS PART OF THE FIRST FUNCTION
-lat = x['results'][0]['geometry']['location']['lat']
-lng = x['results'][0]['geometry']['location']['lng']
-print('lat = %s') % lat 
-print('lng = %s') % lng
+def query_foursquare_recommendation(location_string, search_interest):
 
-#TODO: FORMAT AS A FUNCTION, NOT AS INLINE
+    coord_string = common.parse_coordinates(
+        define_geocode_location(location_string)
+    )
+    fs_parameters = dict(
+        client_id=common.import_foursquare_keys()[0],
+        client_secret=common.import_foursquare_keys()[1],
+        v='20170801',
+        ll=coord_string,
+        query=search_interest,
+        limit=1
+    )
+    response = requests.get(
+        url=common.import_endpoints()[1], params=fs_parameters
+    )
+    result = json.loads(response.text)
+    return(result)
 
-lat_test = '42.29'
-lng_test = '-85.58'
-string_test = '%s,%s' % (lat_test, lng_test)
-#print string_test
-
-params = dict(
-  client_id=common.get_foursquare_keys()[0],
-  client_secret=common.get_foursquare_keys()[1],
-  v='20170801',
-  ll=string_test,
-  query='coffee',
-  limit=1
-)
-resp = requests.get(url=common.get_endpoints()[1], params=params)
-data = json.loads(resp.text)
-print json.dumps(data, sort_keys=True, indent=4)
+x = query_foursquare_recommendation('Kalamazoo,MI','coffee')
+print json.dumps(x, sort_keys=True, indent=4)
